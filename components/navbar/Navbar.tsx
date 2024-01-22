2677771013e287505890cc0b5789ca92154a1843
Exequiel <exe2016gerez@gmail.com>
2024-01-22 03:20:20 -0300

    refactor: add ActiveLink implementation

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import navItems from "./navItems.json";
-import { NavButton } from "./NavButton";
+import { ActiveLink, NavButton } from "..";
 
 export const Navbar = () => {
   return (
@@ -28,15 +28,8 @@ export const Navbar = () => {
           id="navbar-default"
         >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            {navItems.map(({ path, text }) => (
-              <li key={path}>
-                <Link
-                  href={path}
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  {text}
-                </Link>
-              </li>
+            {navItems.map(navItem => (
+              <ActiveLink key={navItem.path} {...navItem}/>
             ))}
           </ul>
         </article>
